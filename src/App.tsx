2391ec6b459e28b6d8e19ae3d7bbe4a7d58f63f0
Amit Chawla <129105171+amitchawla25@users.noreply.github.com>
2026-03-15 21:56:47 -0400

    Remove React import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import ApiKeySetup from './components/ApiKeySetup'
 import ChatInterface from './components/ChatInterface'
 
